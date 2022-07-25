5f147edf9efed7b0e70e023017290bb4f10b3aba
Shaheer Kolta <shaheermehany@gmail.com>
2022-07-25 19:25:55 +0200

    Created Schedule Repository

---
+++ Interfaces/IScheduleRepository.cs
@@ -0,0 +1,14 @@
+﻿using HandyMan.Models;
+
+namespace HandyMan.Interfaces
+{
+    public interface IScheduleRepository
+    {
+        Task<IEnumerable<Schedule>> GetScheduleAsync();
+        Task<Schedule> GetScheduleByHandymanSsnAsync(int id,DateTime date , TimeSpan time);
+        void CreateSchedule(Schedule schedule);
+        void EditSchedule(Schedule schedule);
+        void DeleteScheduleById(int id, DateTime date, TimeSpan time);
+        Task<bool> SaveAllAsync();
+    }
+}
---
+++ Repository/ScheduleRepository.cs
@@ -0,0 +1,52 @@
+﻿using HandyMan.Data;
+using HandyMan.Interfaces;
+using HandyMan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HandyMan.Repository
+{
+    public class ScheduleRepository : IScheduleRepository
+    {
+        private readonly Handyman_DBContext context;
+
+        public ScheduleRepository(Handyman_DBContext context)
+        {
+            this.context = context;
+        }
+
+        public async void CreateSchedule(Schedule schedule)
+        {
+            await context.Schedules.AddAsync(schedule);
+        }
+
+        
+
+        public void DeleteScheduleById(int id, DateTime date, TimeSpan time)
+        {
+            Schedule schedule = context.Schedules.FirstOrDefault(a => a.Handy_SSN == id && a.Schedule_Date.Equals(date) && a.Time_From.Equals(time));
+            context.Remove(schedule);
+        }
+
+        public void EditSchedule(Schedule schedule)
+        {
+            context.Entry(schedule).State = EntityState.Modified;
+        }
+
+        public async Task<IEnumerable<Schedule>> GetScheduleAsync()
+        {
+            return await context.Schedules.ToListAsync();
+        }
+
+        public async Task<Schedule> GetScheduleByHandymanSsnAsync(int id, DateTime date, TimeSpan time)
+        {
+            Schedule schedule = context.Schedules.FirstOrDefault(a => a.Handy_SSN == id && a.Schedule_Date.Equals(date) && a.Time_From.Equals(time));
+            return schedule;
+        }
+
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
